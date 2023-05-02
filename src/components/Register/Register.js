b6f62b560d63c878c565818fe069d1ee96cc1f83
KyangTan <kwanyang29@gmail.com>
2023-05-02 16:16:54 +0800

    Register Page Updated

--- src/components/Register/Register.js
+++ src/components/Register/Register.js
@@ -6,23 +6,37 @@ import {
 import { auth, signInWithGoogle } from "../../api/firebaseconfig";
 import React, { useState, useEffect } from "react";
 import "./Register.scss";
-import { Button, Card, Form, Input, Container, Row, Col, Label} from "reactstrap";
+import {
+  Button,
+  Card,
+  Form,
+  Input,
+  Container,
+  Row,
+  Col,
+  Label,
+} from "reactstrap";
 import { useHistory } from "react-router-dom";
 
 const Register = () => {
   const [registerEmail, setRegisterEmail] = useState("");
   const [registerPassword, setRegisterPassword] = useState("");
-  const [confirmPassword, setConfirmPassword] = useState("");
+  const [registerConfirmPassword, setRegisterConfirmPassword] = useState("");
+  const [registerAddress, setRegisterAddress] = useState("");
+  const [registerGender, setRegisterGender] = useState("");
+  const [registerName, setRegisterName] = useState("");
+  const [registerNationality, setRegisterNationality] = useState("");
   const [user, setUser] = useState({});
   const [emailError, setEmailError] = useState("");
   const [passwordError, setPasswordError] = useState("");
   const [confirmPasswordError, setConfirmPasswordError] = useState("");
+  const [disabled, setDisabled] = useState(true);
   const history = useHistory();
 
   useEffect(() => {
     onAuthStateChanged(auth, (currentUser) => {
       setUser(currentUser);
-      if (currentUser.getIdToken != null) {
+      if (currentUser != null) {
         history.push("/profile");
       }
     });
@@ -64,23 +78,21 @@ const Register = () => {
               <Card className="card-register mx-auto">
                 <h3 className="title mx-auto">Register</h3>
                 <Form className="register-form">
-                  <Label for="error" className="control-label">Email</Label>
+                  <Label for="error" className="control-label">
+                    Email
+                  </Label>
                   {/* <Input type="text" name="error" id="error" defaultValue="Error input"></Input> */}
                   <Input
                     placeholder="Email"
                     type="text"
-                    id="error"
-                    error={!registerEmail}
-                    helperText={registerEmail? emailError : "Do not share your password with anyone."}
-                    value={registerEmail}
                     onChange={(event) => {
                       setRegisterEmail(event.target.value);
-                      if(event.target.value === ""){
+                      if (event.target.value === "") {
                         setEmailError("Email is required");
-                    }else{
+                      } else {
                         setEmailError("");
-                    }
-                  }}
+                      }
+                    }}
                   />
                   <label>Password</label>
                   <Input
@@ -91,7 +103,7 @@ const Register = () => {
                       setRegisterPassword(event.target.value);
                       if (registerPassword === "") {
                         setPasswordError("Password is required");
-                      }else{
+                      } else {
                         setPasswordError("");
                       }
                     }}
@@ -102,20 +114,91 @@ const Register = () => {
                     type="password"
                     helperText={confirmPasswordError}
                     onChange={(event) => {
-                      setConfirmPassword(event.target.value);
-                      if (confirmPassword !== registerPassword) {
+                      setRegisterConfirmPassword(event.target.value);
+                      if (
+                        event.target.value !== registerPassword || registerName === "" || registerAddress === "" || registerEmail === ""
+                      ) {
+                        setDisabled(true);
                         setConfirmPasswordError("Not match password");
-                      }else{
+                      } else {
+                        setDisabled(false);
                         setConfirmPasswordError("");
                       }
                     }}
                   />
+                  <label>Name</label>
+                  <Input
+                    placeholder="Name"
+                    type="text"
+                    onChange={(event) => {setRegisterName(event.target.value)
+                      if (
+                        registerConfirmPassword !== registerPassword || registerName === "" || registerAddress === "" || registerEmail === ""
+                      ) {
+                        setDisabled(true);
+                        setConfirmPasswordError("Not match password");
+                      }else{
+                        setDisabled(false);
+                        setConfirmPasswordError("");
+                      }
+                    }}
+                  />
+                  <label>Address</label>
+                  <Input
+                    placeholder="Address"
+                    type="text"
+                    onChange={(event) => {setRegisterAddress(event.target.value)
+                      console.log("registerNationality: "+ registerNationality)
+                      console.log("registerGender: "+ registerGender)
+                      console.log("registerAddress: "+ registerAddress)
+                      console.log("registerName: "+ registerName)
+                      console.log("registerConfirmPassword: "+ registerConfirmPassword)
+                      console.log("registerPassword: "+ registerPassword)
+                      console.log("registerEmail: "+ registerEmail)
+                      
+                      if (
+                        registerConfirmPassword !== registerPassword || registerName === "" || registerAddress === "" || registerEmail === ""
+                      ) {
+                        setDisabled(true);
+                        setConfirmPasswordError("Not match password");
+                      }else{
+                        setDisabled(false);
+                        setConfirmPasswordError("");
+                      }}}
+                  />
+                  <label>Nationality</label>
+                  <Input type="select" name="select" id="inputState">
+                    <option>Malaysia</option>
+                    <option>United State of America</option>
+                    <option>Japan</option>
+                    <option>United Kingdom</option>
+                    <option>China</option>
+                    <option>India</option>
+                    <option>Indonesia</option>
+                    <option>Thailand</option>
+                    <option>Philippines</option>
+                    <option>Vietnam</option>
+                    <option>Singapore</option>
+                    <option>Myanmar</option>
+                    <option>South Korea</option>
+                    <option>Brunei</option>
+
+                    onChange={(event) => {setRegisterNationality(event.target.value)
+                    }}
+                  </Input>
+                  <label>Gender</label>
+                  <Input type="select" name="select" id="inputState">
+                    <option>Male</option>
+                    <option>Female</option>
+                    <option>Prefer not to say</option>
+
+                    onChange={(event) => {setRegisterGender(event.target.value)}}
+                  </Input>
                   <Button
                     block
                     className="registerBtn"
+                    disabled={disabled}
                     // color="primary"
-                    onClick={registerWithEmail
-                    }
+                    onClick={registerWithEmail}
                   >
                     Register
                   </Button>
