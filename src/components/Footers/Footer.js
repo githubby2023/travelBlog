f1c53eb90b6ee595d4f472f3a498192a4f8497ff
mingng18 <92716196+mingng18@users.noreply.github.com>
2023-06-21 03:04:45 +0800

    Fix footer

--- src/components/Footers/Footer.js
+++ src/components/Footers/Footer.js
@@ -1,16 +1,19 @@
 import React from "react";
+import "./Footer.scss";
 
 function Footer() {
   return (
     <footer className="footer-container">
       <div className="container">
         <div className="row">
-          <h6 className="">Travel Blog</h6>
-          <div className="credits ml-auto">
-            <span className="copyright">
-              © {new Date().getFullYear()}, made with{" "}
-              <i className="fa fa-heart heart" />
-            </span>
+          <div className="bruh">
+            <h6 className="">Travel Blog</h6>
+            <div className="credits ml-auto">
+              <span className="copyright">
+                © {new Date().getFullYear()}, made with{" "}
+                <i className="fa fa-heart heart" />
+              </span>
+            </div>
           </div>
         </div>
       </div>
